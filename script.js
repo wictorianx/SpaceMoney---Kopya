270555fe71d2657ec1e2cfef4739969fa8e66571
wictorianx <88106599+wictorianx@users.noreply.github.com>
2022-08-28 02:08:51 +0300

    bug

--- script.js
+++ script.js
@@ -11,15 +11,22 @@ class business {
         this.cpsData = cpsData;
         this.button = button;
     }
+    calculate() {
+        console.log(this)
+        this.amount+=1
+        this.price*=this.cpsData+1
+        this.display.textContent=amount
+        console.log(this.name)
+    }
 }
 
 let alien = new business("Alien",document.querySelector(".alien"),0,1,1.1,document.querySelector("#alienImg"))
 let astreoid_mine = new business("Astreoid Mine",document.querySelector(".astreoid_mine"),0,100,1.2,document.querySelector("#astreoid_mineImg"))
-let energy = new business("Energy",document.querySelector(".energy"),0,1000,1.3),document.querySelector("#energyImg")
+let energy = new business("Energy",document.querySelector(".energy"),0,1000,1.3,document.querySelector("#energyImg"))
 let research = new business("Research",document.querySelector(".research"),0,10000,1.4,document.querySelector("#researchImg"))
 let rocket = new business("Rocket",document.querySelector(".rocket"),0,100000,1.5,document.querySelector("#rocketImg"))
 let satellite = new business("Satellite",document.querySelector(".satellite"),0,1000000,1.6,document.querySelector("#satelliteImg"))
-let space_elevator = new business("Space Elevator",document.querySelector(".space_elevator"),0,10000000,1.7,document.querySelector("space_elevator#Img"))
+let space_elevator = new business("Space Elevator",document.querySelector(".space_elevator"),0,10000000,1.7,document.querySelector("#space_elevatorImg"))
 let space_tourist = new business("Space Tourist",document.querySelector(".space_tourist"),0,100000000,1.8,document.querySelector("#space_touristImg"))
 
 biz = [
@@ -35,5 +42,7 @@ biz = [
 
 for(let i = 0; i<biz.length;i++){
     biz[i].display.textContent = biz[i].name
+    biz[i].button.onclick= biz[i].calculate
+    console.log(biz[i])
 }
 
